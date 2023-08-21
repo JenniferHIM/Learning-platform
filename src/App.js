fac0fc1d56ea1defb402be5404292ef565f8e7bd
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2023-08-21 18:53:21 +0200

    basic commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Learning platform</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
